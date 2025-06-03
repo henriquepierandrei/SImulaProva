bee662a737552257d2286741aea5afd93f21bdf4
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2025-06-03 08:14:34 -0300

    primeiro

---
+++ src/main/java/tech/pierandrei/SimulaProva/SimulaProvaApplication.java
@@ -0,0 +1,13 @@
+package tech.pierandrei.SimulaProva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulaProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulaProvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/pierandrei/SimulaProva/SimulaProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pierandrei.SimulaProva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulaProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
