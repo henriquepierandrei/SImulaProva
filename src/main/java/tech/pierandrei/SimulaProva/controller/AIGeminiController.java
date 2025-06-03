9b753c6193129f306fa7a78a89cf112b3f643b54
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2025-06-03 14:36:19 -0300

    feat: Novas feats

--- src/main/java/tech/pierandrei/SimulaProva/controller/AIGeminiController.java
+++ src/main/java/tech/pierandrei/SimulaProva/controller/AIGeminiController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tech.pierandrei.SimulaProva.config.AIGeminiConfig;
 import tech.pierandrei.SimulaProva.dto.RequestDto;
 import tech.pierandrei.SimulaProva.service.AIGeminiService;
 
