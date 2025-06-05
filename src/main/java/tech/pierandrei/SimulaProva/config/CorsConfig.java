5efae7c57f103a50c621d3fdd4aa89991d480406
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2025-06-04 21:10:48 -0300

    refactor: Atualizado a url do frontend

--- src/main/java/tech/pierandrei/SimulaProva/config/CorsConfig.java
+++ src/main/java/tech/pierandrei/SimulaProva/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://simulaprova.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
